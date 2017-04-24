ab7d17f162e1bcf0ca7f9a50f7567910b5987ff6
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2017-04-25 00:58:21 +0200

    now only output text

--- main.cxx
+++ main.cxx
@@ -5,12 +5,13 @@
 
 int main(int argc, char** argv)
 {
-  auto x = std::make_shared<std::string>("DDDDDD");
-  std::cout << "hello from clang?" << std::endl;
-  std::cout << "smstring: " << *x << std::endl;
-  std::shared_ptr<std::vector<int>> intVecShPtr(new std::vector<int>());
-  intVecShPtr->push_back(1);
+  //auto x = std::make_shared<std::string>("DDDDDD");
+  //std::cout << "hello from clang?" << std::endl;
+  //std::cout << "smstring: " << *x << std::endl;
+  //std::shared_ptr<std::vector<int>> intVecShPtr(new std::vector<int>());
+  //intVecShPtr->push_back(1);
 
-  std::cout << "int vec 1: " << std::to_string(intVecShPtr->at(0)) << std::endl;
+  //std::cout << "int vec 1: " << std::to_string(intVecShPtr->at(0)) << std::endl;
+  std::cout << "hello from bob cross compiled?" << std::endl;
   return 0;
 }
