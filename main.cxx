267bdabfd92830faec7d0ccaf4d94b4497365a6e
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2017-04-25 23:16:59 +0200

    enabled full program again

--- main.cxx
+++ main.cxx
@@ -5,13 +5,11 @@
 
 int main(int argc, char** argv)
 {
-  //auto x = std::make_shared<std::string>("DDDDDD");
-  //std::cout << "hello from clang?" << std::endl;
-  //std::cout << "smstring: " << *x << std::endl;
-  //std::shared_ptr<std::vector<int>> intVecShPtr(new std::vector<int>());
-  //intVecShPtr->push_back(1);
-
-  //std::cout << "int vec 1: " << std::to_string(intVecShPtr->at(0)) << std::endl;
-  std::cout << "hello from bob cross compiled?" << std::endl;
+  std::cout << "hello from bob cross compiled!!!" << std::endl;
+  auto x = std::make_shared<std::string>("now execute some advanced C++14 stuff ...");
+  std::cout << "smstring: " << *x << std::endl;
+  std::shared_ptr<std::vector<int>> intVecShPtr(new std::vector<int>());
+  intVecShPtr->push_back(1);
+  std::cout << "int vec 1: " << std::to_string(intVecShPtr->at(0)) << std::endl;
   return 0;
 }
